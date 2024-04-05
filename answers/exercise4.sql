db6d42e93823fef93402f8b54aa8b0190951331c
Mariyana-Katz <mariqnak21@gmail.com>
2024-04-05 12:57:43 -0400

    finished

---
+++ answers/exercise4.sql
@@ -0,0 +1,6 @@
+List the number of Students in each Country.
+
+SELECT COUNT(Students), Country FROM Students GROUP BY Country;
+
+
+
---
+++ answers/exercise6.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(StudentsID), Country FROM Students GROUP BY Country HAVING COUNT(StudentsID) > 10 ORDER BY COUNT(StudentsID) DESK;
